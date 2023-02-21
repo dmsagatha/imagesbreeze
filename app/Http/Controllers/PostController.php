01867f359cb5ad49e97dca14cd2492b7f98f3e75
Doris Morales <dmorales55@gmail.com>
2023-02-20 22:45:06 -0500

    Post - Crear: Imagen con Filepond

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\TemporaryFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+  public function index()
+  {
+    $posts = Post::all();
+
+    return view('posts.post_create', compact('posts'));
+  }
+  
+  public function create()
+  {
+  }
+  
+  public function store(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'title' => 'required',
+    ]);
+
+    $temp_file = TemporaryFile::where('folder', $request->image)->first();
+
+    if ($validator->fails() && $temp_file)
+    {
+      Storage::deleteDirectory('posts/tmp/' . $temp_file->folder);
+      $temp_file->delete();
+
+      return to_route('posts.index')->withErrors($validator)->withInput();
+    }
+    elseif ($validator->fails())
+    {
+      return to_route('posts.index')->withErrors($validator)->withInput();
+    }
+
+    if ($temp_file)
+    {
+      Storage::copy('posts/tmp/' . $temp_file->folder . '/' . $temp_file->file, 'posts/' . $temp_file->folder . '/' . $temp_file->file);
+
+      Post::create([
+        'title' => $request->title,
+        'image' => $temp_file->folder . '/' . $temp_file->file,
+      ]);
+      Storage::deleteDirectory('posts/tmp/' . $temp_file->folder);
+      $temp_file->delete();
+
+      return to_route('posts.index')->with('success', 'Post Created');
+    }
+    else
+    {
+      return to_route('posts.index')->with('error', 'Please uplaod an image');
+    }
+  }
+
+  public function tempUplaod(Request $request)
+  {
+    if ($request->hasFile('image'))
+    {
+      $image     = $request->file('image');
+      $file_name = $image->getClientOriginalName();
+      $folder    = uniqid('post', true);
+      $image->storeAs('posts/tmp/' . $folder, $file_name);
+      TemporaryFile::create([
+        'folder' => $folder,
+        'file'   => $file_name,
+      ]);
+
+      return $folder;
+    }
+    else
+    {
+      return '';
+    }
+  }
+
+  public function tempDelete()
+  {
+    $temp_file = TemporaryFile::where('folder', request()->getContent())->first();
+    
+    if ($temp_file)
+    {
+      Storage::deleteDirectory('public/posts/tmp/' . $temp_file->folder);
+      $temp_file->delete();
+
+      return response('');
+    }
+  }
+}
